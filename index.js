3bfd640b9880868ed154583b3837ec95041006e5
Ryan McCarvill <ryanmccarvill@Ryans-MBP.telecom>
2016-06-23 19:58:31 +1200

    added delete functionality and onchange event handling

--- index.js
+++ index.js
@@ -3,38 +3,82 @@ class CRDTString {
 		this.start = new CRDTSpan( "" , - 1 );
 		this.end = new CRDTSpan( "" , -2 );
 
-		this.crdt = new CRDTSpan( str , 0 );
+		this.crdt = new CRDTSpan( str , 1 );
 
 		this.start.joinSpans( this.start , this.crdt );
 		this.end.joinSpans( this.crdt , this.end );
+
+		this.length = str.length;
+		this.events = [ ];
 	}
 
 	split( offset ) {
 		this.start.split( offset );
 	}
 
+	splitAtId( id ) {
+		this.start.splitAtId( id );
+	}
+
+
+	delete( offset , length ) {
+		let action = this.start.delete( offset , length );
+		this.length -= length;
+		this.fireEvents( action );
+		return action;
+	}
+
 	insert( offset , character ) {
-		this.start.insert( offset , character );
+		let action = this.start.insert( offset , character );
+		this.length++;
+		this.fireEvents( action );
+		return action; 
+
 	}
 
-	toString( ) {
+	onChange( cb ) {
+		return( this.events.push( cb ) );
+	}
+
+	fireEvents( event ) {
+		this.events.forEach( ( item ) => {
+		//	process.nextTick( ( ) => {
+				item( event );
+		//	 });
+		} );
+	}
+
+	toString( showHidden ) {
 		let str = "";
 		let item = this.start.next;
 		while( item !== this.end ) {
-			str += item.str;
+			if( item.isVisible || showHidden ) {
+				str += item.str;	
+			}
 			item = item.next;
 		}
 		return str;
 	}
-	toSomething( ) {
+	toDebug( ) {
 		let str = "";
 		let item = this.start.next;
 		while( item !== this.end ) {
-			str += "{id:" + item.id + ",str:" + item.str + ",length:" + item.length + "},";
+			str += "{id:" + item.id + ",str:" + item.str + ",length:" + item.length + ",isNew:" + item.isNew + ",isVisible:" + item.isVisible + "},";
 			item = item.next;
 		}
 
 		return str;
+	}
+	toId( ) {
+		let ids = [ ];
+		let item = this.start.next;
+		while( item !== this.end ) {
+		
+			ids.push( item.id );
+			item = item.next
+		}
+		console.log( ids );
+		return ids;
 	}
 }
 
@@ -45,58 +89,140 @@ class CRDTSpan {
 		this.str 	= str;
 		this.id		= id;
 		this.length = str.length;
+		this.isVisible = true;
 		this.isNew 	= isNew || false;
+
 	}
 
 	insert( offset , character ) {
 		
 		let { left , right } = this.split( offset );
 		
-		let left_offset_length = left.isNew ? 0 : left.id + left.length;
+		let left_offset_length = left.isNew ? left.id : left.id + left.length;
+
+		if( left.isNew ) {
+			//console.log( right.id , left.id , )
+		}
+
+		let id = ( ( right.id - left_offset_length ) / 2 ) + left_offset_length;
 		
-		let span = new CRDTSpan( character , ( ( right.id - left_offset_length ) / 2 ) + left_offset_length , true );
+		//we can't halve 0
+		if( right.id === left_offset_length ) {
+			id = right.id - .5;
+		}
+		
+		
+		let span = new CRDTSpan( character , id, true );
 
 		this.joinSpans( left , span );
 		this.joinSpans( span , right );
-		
+
+		return { verb : 'add' , operation : span.serialise( ) }
 	}
+
+	delete( offset , length ) {
+		length = length || 1;
+		let start = this.split( offset );
+		let end = start.right.split( length );
+
+		let item = start.left.next;
+		while( item != end.left ) {
+			item.isVisible = false;
+			item = item.next;
+		}
+		item.isVisible = false;
+
+		return { verb : 'del' , operation : item.serialise( ) }
+	}
+
 	split( offset ) {
 		
 		let total = this.length;
 		let item = this;
 		
 		while( offset > total && item.id !== -2 ) {	
-			
  			item = item.next;
 			total += item.length;		
- 			
- 			
-		}
-
-		if( item.id == -2 ) {
-			throw new Error( "Out of bounds." );
 		}
 		
 		let charOffset = item.length - (total - offset );
 		
 
+		if( item.id == -2  ) { //after end
+			item = item.prev;
+			charOffset = item.length;
+		} else if ( item.id == -1 ) { //before start
+			item = item.next;
+			charOffset = 0;
+		}
+
+
 		let left = new CRDTSpan( item.str.substr( 0 , charOffset ) , item.id );
 		let right = new CRDTSpan( item.str.substr( charOffset ) , ( item.id + charOffset ) + 1 );
 
-		//need to get rid of left or right if they are null //todo
 
-		this.joinSpans( item.prev , left );
-		this.joinSpans( right , item.next );
-		this.joinSpans( left , right );
+		//also copy new, ownership, and tombstone info
 		
-		return { 
-			left , right
+		left.isNew = item.isNew;
+		right.isNew = item.isNew;
+		left.isVisible = item.isVisible;
+		right.isVisible = item.isVisible;
+
+		if( left.length === 0 ) {
+			this.joinSpans( item.prev , right );
+			this.joinSpans( right , item.next );
+			return { left : right.prev , right }
+		} else if (right.length === 0 ) {
+			this.joinSpans( item.prev , left );
+			this.joinSpans( left , item.next );
+			return { left , right : left.next }
+		} else {
+			this.joinSpans( item.prev , left );
+			this.joinSpans( right , item.next );
+			this.joinSpans( left , right );		
+			return { 
+				left , right
+			}
 		}
+	}
+
+
+
+	insertAtId( id ) {
+
+		//if the id is a fraction then it's a newly inserted character
+		
+		if( id !== Math.round( id ) ) {
+			let item = this;
+			while( item.id < id ) {
+				item = item.next;
+			}
+
+			let span = new CRDTSpan( character , ( ( right.id - left_offset_length ) / 2 ) + left_offset_length , true );
+
+			this.joinSpans( item , span );
+			return;
+		}
+	}
+
+	splitAtId( id ) {
+		let item = this;
+		while( item.id + item.length < id ) {
+			item = item.next;
+		}
+
+		console.log( item.id );
 	}
 
 	joinSpans( startSpan , endSpan ) {
 		startSpan.next = endSpan;
 		endSpan.prev = startSpan;
+	}
+
+	serialise( ) {
+		return {
+			id : this.id, str : this.str, visible : this.isVisible
+		};
 	}
 
 }
--- test/test.js
+++ test/test.js
@@ -3,19 +3,131 @@ var CRDT = require("../index.js");
 
 
 
-let crdt = CRDT( "TEST STRING" );
-//crdt.split( 2 );
-//console.log( crdt.toSomething( ) );
 
-crdt.insert( 2 , "W" );
-console.log( crdt.toSomething( ) );
-//crdt.split( 3 );
-crdt.insert( 7 , "X" );
-console.log( crdt.toSomething( ) );
-crdt.insert( 8 , "Y" );
-console.log( crdt.toSomething( ) );
-crdt.insert( 9 , "Z" );
-console.log( crdt.toSomething( ) );
+describe( "CRDT insertion and deletion tests" , ( ) => {
+	
+		
+		it('The CRDT should insert in a predictable and logical fashion', ( done ) => {
 
-console.log( crdt.toString( ) );
- 
+
+			let testString = "TEST STRING";
+			let crdt = CRDT( testString );
+
+
+			crdt.insert( 2 , "W" );
+			testString = stringSplice( testString , 2 , 0 , "W" );
+
+			crdt.insert( 7 , "X" );
+			testString = stringSplice( testString , 7 , 0 , "X" );
+
+			crdt.insert( 8 , "Y" );
+			testString = stringSplice( testString , 8 , 0 , "Y" );
+
+			crdt.insert( 9 , "Z" );
+			testString = stringSplice( testString , 9 , 0 , "Z" );
+
+			crdt.insert( 8 , "#" );
+			testString = stringSplice( testString , 8 , 0 , "#" );
+
+			crdt.insert( 0 , "A" );
+			testString = stringSplice( testString , 0 , 0 , "A" );
+
+			crdt.insert( crdt.length, "Z" );
+			testString = stringSplice( testString , testString.length , 0 , "Z" );
+
+			assert.equal( crdt.toString( ) , testString );
+			done( );
+			 
+		} );
+
+		it( 'The CRDT should delete in a predictable and logical fashion' , ( done ) => {
+			let testString = "TEST STRING";
+			let crdt = CRDT( testString );
+
+			crdt.delete( 2 , 3 );
+			testString = stringSplice( testString , 2 , 3 );
+			assert.equal( crdt.toString( ) , testString );
+			done();
+		} );
+
+		it( 'should survive a stress test' , ( done ) => {
+
+			let testString = "TEST STRING";
+			let crdt = CRDT( testString );
+
+			for( let i = 0; i < 10000; i++ ) {
+				let insert = Math.floor( Math.random(  ) * 2 );
+				let character = Math.random().toString(36).substr(3 , 1);
+				let position = Math.floor( Math.random( ) * crdt.length );
+				let length = Math.floor( Math.random( ) * 10 );
+				if( insert ) {
+					crdt.insert( position , character );
+					testString = stringSplice( testString , position , 0 , character );
+				} else {
+					if( length == 0 || position ==0) break;
+					crdt.delete( position  , length  );
+					testString = stringSplice( testString , position , length );
+
+				}
+
+			}
+			assert.equal( crdt.toString( ) , testString );
+
+			//
+			
+		//	console.log( crdt.toId( ) );
+			done();
+		} );
+
+	});
+describe( 'synchronisation' , ( ) => {
+	it( 'two CRDT strings should remain in sync with replays ' , ( done ) => {
+		done();
+	});
+
+	it( 'if two concurrent operations occur the CRDT should maintain intention' , ( done ) => {
+		done();
+	});
+});
+
+describe( 'events' , ( ) => {
+	it( 'should recieve events' , ( done ) => {
+			let testString = "TEST STRING";
+			let crdt = CRDT( testString );
+			let actionCount = 0;
+			let sentCount =0;
+			crdt.onChange( ( e ) => {
+				actionCount++;
+			});
+
+			for( let i = 0; i < 1000; i++ ) {
+				let insert = Math.floor( Math.random(  ) * 2 );
+				let character = Math.random().toString(36).substr(3 , 1);
+				let position = Math.floor( Math.random( ) * crdt.length );
+				let length = Math.floor( Math.random( ) * 10 );
+				if( insert ) {
+					crdt.insert( position , character );
+					testString = stringSplice( testString , position , 0 , character );
+					sentCount++;
+				} else {
+					if( length == 0 || position ==0) break;
+					sentCount++;
+					crdt.delete( position  , length  );
+					testString = stringSplice( testString , position , length );
+
+				}
+
+			}
+
+			assert.equal( actionCount , sentCount );
+			done();
+	} );
+} );
+ //
+ //crdt.splitAtId( 7 );
+ //
+ //
+ function stringSplice( str , start , delcount , newstr ) {
+ 	newstr = newstr || "";
+ 	return str.slice( 0 , start ) + newstr + str.slice( start + delcount );
+ }
